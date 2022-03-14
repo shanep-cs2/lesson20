830410937af99f24ac4ee818f1adda8612ba8957
Shane K. Panter <shane@foundationcode.com>
2022-03-14 13:01:02 -0600

    Update Palindrome.java

--- app/src/main/java/lesson/Palindrome.java
+++ app/src/main/java/lesson/Palindrome.java
@@ -8,7 +8,7 @@ public interface Palindrome{
 
     /**
      * Check to see if the given string is a palindrome. This method ignores punctuation and 
-     * spacing and treats all letters as case insensitive. 
+     * spacing and treats all letters and numbers as case insensitive. 
      * A one-character string is a palindrome.
      * @param p The string to check
      * @return true is the string is a palindrome false otherwise.
